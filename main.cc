c2a9d636f3974373de72582b8a907b300bdc78e0
Arrielle Opotowsky <arrielle.opotowsky@gmail.com>
2014-11-11 16:28:00 -0600

    trying...

--- main.cc
+++ main.cc
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <cyclus.h>
+#include <map>
 #include <sqlite_back.h>
+#include "prettyprint.hpp"
 
 int main(int argc, char* argv[]) {
   using std::cout;
-  cout << "Derp\n";
-  cyclus::toolkit::ExponentialFunction* func = new cyclus::toolkit::ExponentialFunction(10.0, -1.0, 0.0);
-  cout << func->Print() << "\n";
-  delete func;
   std::string table = "Compositions";
   std::string fname = std::string(argv[1]);
   cout << "file name: " << fname << "\n";
   cyclus::FullBackend* fback = new cyclus::SqliteBack(fname);
   cyclus::QueryResult result = fback->Query(table, NULL);
+  std::vector<std::string> fld = result.fields;
+  cout << fld << std::endl;
+  cout << result << std::endl;
   delete fback;
   return 0;
 }
