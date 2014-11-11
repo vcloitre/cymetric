581276d3e9e85a4f7e7f2e06eae45d6238c09a3b
Arrielle Opotowsky <arrielle.opotowsky@gmail.com>
2014-11-11 13:54:08 -0600

    testing sqlite db access

--- main.cc
+++ main.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cyclus.h>
-#include <hdf5_back.h>
+#include <sqlite_back.h>
 
 int main(int argc, char* argv[]) {
   using std::cout;
@@ -11,7 +11,7 @@ int main(int argc, char* argv[]) {
   std::string table = "Compositions";
   std::string fname = std::string(argv[1]);
   cout << "file name: " << fname << "\n";
-  cyclus::FullBackend* fback = new cyclus::Hdf5Back(fname);
+  cyclus::FullBackend* fback = new cyclus::SqliteBack(fname);
   cyclus::QueryResult result = fback->Query(table, NULL);
   delete fback;
   return 0;
