51c90052188bc4546cba1ddb39f0757f972100af
Arrielle Opotowsky <arrielle.opotowsky@gmail.com>
2014-11-11 13:48:42 -0600

    found the line that causes the seg fault but do not understand why

--- main.cc
+++ main.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cyclus.h>
 #include <hdf5_back.h>
+#include <sqlite_back.h>
 
 int main(int argc, char* argv[]) {
   using std::cout;
@@ -12,7 +13,7 @@ int main(int argc, char* argv[]) {
   std::string fname = std::string(argv[1]);
   cout << "file name: " << fname << "\n";
   cyclus::FullBackend* fback = new cyclus::Hdf5Back(fname);
-  cyclus::QueryResult result = fback->Query(table, NULL);
+//  cyclus::QueryResult result = fback->Query(table, NULL);
   delete fback;
   return 0;
 }
