0519ea1711dc7501d844af98ef9117bd7bad2dc1
opotowsky <arrielle.opotowsky@gmail.com>
2014-11-17 10:30:23 -0600

    refactoring data type access in script - does not yet work

--- main.cc
+++ main.cc
@@ -4,33 +4,35 @@
 #include <sqlite_back.h>
 #include "prettyprint.hpp"
 
-void dbtype(cyclus::DbTypes t, void* x) {
-  switch(t){
+std::string ToStr(cyclus::DbTypes type, boost::spirit::hold_any val) {
+  std::stringstream ss;
+  switch(type){
     case 0:
-      (bool*)x;
+      ss << val.cast<bool>();
       break;
     case 1:
-      (int*)x;
+      ss << val.cast<int>();
       break;
     case 2:
-      (float*)x;
+      ss << val.cast<float>();
       break;
     case 3:
-      (double*)x;
+      ss << val.cast<double>();
       break;
     case 4:
-      (std::string*)x;
+      ss << val.cast<std::string>();
       break;
     case 5:
-      (cyclus::vl_string*)x;
+      ss << val.cast<std::string>();
       break;
-    case 6:
-      (cyclus::Blob*)x;
-      break;
+//    case 6:
+//      ss << val.cast<cyclus::Blob>();
+//      break;
     case 7:
-      (boost::uuids::uuid*)x;
+      ss << val.cast<boost::uuids::uuid>();
       break;
   }
+  return ss.str();
 }
 
 int main(int argc, char* argv[]) {
@@ -41,15 +43,19 @@ int main(int argc, char* argv[]) {
   cout << "table name: " << table << "\n";
   cyclus::FullBackend* fback = new cyclus::SqliteBack(fname);
   cyclus::QueryResult result = fback->Query(table, NULL);
-  cout << "\n" << "SimID: " << result.GetVal<boost::uuids::uuid>("SimId", 0) << "\n\n";
+  cout << "\n" << "SimID: "; 
+  cout << result.GetVal<boost::uuids::uuid>("SimId", 0) << "\n\n";
   std::vector<std::string> cols = result.fields;
-  for (int i = 0; i < result.rows.size(); ++i) {
-//    cout << result.GetVal<int>(cols[2], i);
-    for (int j = 0; i < cols.size(); ++j) {
-      int e = result.types[j];
-      void dtype = dbtype(e);
-      cout << result.GetVal<dtype>(cols[j], i);
-    }
+  std::vector<cyclus::QueryRow> lines = result.rows;
+  for (int i = 0; i < lines.size(); ++i) {
+    cout << lines << "\n";
+
+//    for (int j = 0; j < cols.size(); ++j) {
+//     cyclus::DbTypes type = result.types[j];
+//      boost::spirit::hold_any val = "";
+//      std::string dtype = ToStr(type, val);
+//      cout << result.GetVal<dtype>(cols[j], i);
+//    }
   }
   delete fback;
   return 0;
