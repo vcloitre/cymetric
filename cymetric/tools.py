710e203be35172201ca6c9d85019a6800b07c022
vcloitre <cloitre@wisc.edu>
2015-07-28 14:04:40 -0500

    allow tuple as dependancies

--- cymetric/tools.py
+++ cymetric/tools.py
@@ -25,7 +25,10 @@ def dbopen(fname):
 
 def raw_to_series(df, idx, val):
     """Convert data frame to series with multi-index."""
-    d = df.set_index(list(map(str, idx)))
+    if isinstance(idx[0], tuple):
+        d = df.set_index(list(map(tuple, idx)))
+    else:
+        d = df.set_index(list(map(str, idx)))
     s = df[val].copy()
     s.index = d.index
     return s
